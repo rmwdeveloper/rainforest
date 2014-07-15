f180b6d67d16e9672ba8ec95cd632f6f0d026fc0
rmwdeveloper <rmwdeveloper@gmail.com>
2014-07-15 16:42:26 -0400

    Added models to portfolio_listing

--- mysite/portfolio_listing/models.py
+++ mysite/portfolio_listing/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+	"""
+	Main table for a portfolio project.
+	"""
+	project_id = models.AutoField(primary_key=True)
+	name = models.CharField()
+	description = models.CharField()
+	date_finished = models.Datefield()
+	date_posted = models.DateTimeField()
+
+class ProjectTypeValidation(models.model):
+	"""Validation Table """
+	project_type_id = models.AutoField(primary_key=True)
+	name = models.CharField()
+	description = models.CharField()
+		
+
+class ProjectType(models.Model):
+	"""
+	Linking table between Project and ProjectTypeValidation
+	"""
+	project_id = models.ForeignKey(Project)
+	project_type_id = models.ForeignKey(ProjectTypeValidation)
+
+
--- mysite/portfolio_listing/tests.py
+++ mysite/portfolio_listing/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from models import Project, ProjectTypeValidation, ProjectType
+import datetime
+import random
+from mysite import settings
 
 # Create your tests here.
+class TestViewConnections(TestCase):
+	"""
+	Tests all view connections in portfolio_listing
+	"""
+	def run_connection_test(self, url_string):
+		"""
+		Tests the response to a particular url
+		"""
+		response = self.client.get(url_string)
+		self.assertEqual(response.status_code, 200 , url_string + ' is not connecting properly')
+
+	def test_views_connections_noauth(self):
+		"""
+		Test all connections that do not require a login 
+		"""
+		url_string_list = ["portfolio",]
+		for url in url_string_list:
+			self.run_connection_test(url)
+

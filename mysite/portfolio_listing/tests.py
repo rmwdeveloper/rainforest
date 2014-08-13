2923c16f40359b4340d3db07007bfbe0d7f1af89
Robert Westenberger <rmwdeveloper@gmail.com>
2014-08-13 18:38:07 -0400

    ADded test for page with required authorization

--- mysite/portfolio_listing/tests.py
+++ mysite/portfolio_listing/tests.py
@@ -9,7 +9,7 @@ class TestViewConnections(TestCase):
 	"""
 	Tests all view connections in portfolio_listing
 	"""
-	def run_connection_test(self, url_string):
+	def run_connection_test(self, url_string ,aut):
 		"""
 		Tests the response to a particular url
 		"""
@@ -20,7 +20,17 @@ class TestViewConnections(TestCase):
 		"""
 		Test all connections that do not require a login 
 		"""
-		url_string_list = ["/portfolio/",]
+		url_string_list = ["/portfolio/", ]
+		for url in url_string_list:
+			self.run_connection_test(url)
+	def test_views_connections_auth(self):
+		"""
+		Test all connections that require a login
+		"""
+		url_string_list = ["/portfolio-edit/", ]
+		my_admin = User.objects.create_superuser('myuser', 'myemail@test.com', 'password')
+		self.client.login(username='myuser', password='password')
+		User.objects.create_user
 		for url in url_string_list:
 			self.run_connection_test(url)
 

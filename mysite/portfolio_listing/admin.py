8baafce2d835daac02ced17f1bfb15c5535d6f19
Robert Westenberger <rmwdeveloper@gmail.com>
2014-08-13 20:24:07 -0400

    Added fixtures to prepopulate database

--- mysite/portfolio_listing/admin.py
+++ mysite/portfolio_listing/admin.py
@@ -1,7 +1,8 @@
 from django import forms
 from django.contrib import admin
-from django.contrib.auth.models import Group
-from django.contrib.auth.admin import UserAdmin
-from django.contrib.auth.forms import ReadOnlyPasswordHashField
 
+from models import Project
 
+class ProjectAdmin(admin.ModelAdmin):
+	pass
+admin.site.register(Project, ProjectAdmin)

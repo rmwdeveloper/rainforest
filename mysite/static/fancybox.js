208e3ba1f1a93536311fd8f20485531e8f2fdf4b
Robert Westenberger <rmwdeveloper@gmail.com>
2014-09-03 17:03:55 -0400

    Finalized fancybox options

--- mysite/static/fancybox.js
+++ mysite/static/fancybox.js
@@ -2,7 +2,15 @@ $(document).ready(function() {
 	$(".fancybox").fancybox({
 		openEffect	: 'none',
 		closeEffect	: 'none',
-		height: '324',
-		width: '537', 
+		scrolling: 'no',
+		// height: '324',
+		// width: '537', 
+		autoCenter: true,
+		// aspectRatio: true,
+		fitToView: true,
+		autoResize: true,
+		helpers : {
+			buttons: {},
+		}
 	});
 });

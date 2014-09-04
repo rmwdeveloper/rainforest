5da9d4e3aa34206fcf3ee52d0d03d865ceaf8c33
Robert Westenberger <rmwdeveloper@gmail.com>
2014-09-04 17:51:06 -0400

    Changed navbar to bootstrap markup and changed fixed jquery modal

--- mysite/static/login_modal.js
+++ mysite/static/login_modal.js
@@ -1,33 +1,13 @@
-// $(document).ready(function() {
-// $('a.login-window').click(function() {
-    
-//             //Getting the variable's value from a link 
-//     var loginBox = $(this).attr('href');
+$(document).ready(function() {
+	$('.login-window').click(function(){
 
-//     //Fade in the Popup
-//     $(loginBox).fadeIn(300);
-    
-//     //Set the center alignment padding + border see css style
-//     var popMargTop = ($(loginBox).height() + 24) / 2; 
-//     var popMargLeft = ($(loginBox).width() + 24) / 2; 
-    
-//     $(loginBox).css({ 
-//         'margin-top' : -popMargTop,
-//         'margin-left' : -popMargLeft
-//     });
-    
-//     // Add the mask to body
-//     $('body').append('<div id="mask"></div>');
-//     $('#mask').fadeIn(300);
-    
-//     return false;
-// });
+		if($('.ui-dialog').css('display') === 'block'){
+			$('#login_form').dialog('close');
+		}
+		
+		else{
+			$('#login_form').dialog();
+		}
 
-// // When clicking on the button close or the mask layer the popup closed
-// $('a.close, #mask').live('click', function() { 
-//   $('#mask , .login-popup').fadeOut(300 , function() {
-//     $('#mask').remove();  
-// }); 
-// return false;
-// });
-// });
+	});
+});

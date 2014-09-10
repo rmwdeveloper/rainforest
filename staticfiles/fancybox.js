f8340846e77ba1a38247b3cac18dca1d70f90bcb
Robert Westenberger <rmwdeveloper@gmail.com>
2014-09-10 18:41:28 -0400

    Changed staticfile settings

---
+++ staticfiles/fancybox.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+	$(".fancybox").fancybox({
+		openEffect	: 'none',
+		closeEffect	: 'none',
+		scrolling: 'no',
+		// height: '324',
+		// width: '537', 
+		autoCenter: true,
+		// aspectRatio: true,
+		fitToView: true,
+		autoResize: true,
+		helpers : {
+			buttons: {},
+		}
+	});
+});

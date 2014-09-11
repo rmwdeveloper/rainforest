57320a9f2724e9ab8ba547d1e79ed4714e3d604f
Robert Westenberger <rmwdeveloper@gmail.com>
2014-09-11 17:08:49 -0400

    Added blog coming soon link

---
+++ static/blog_coming_soon.js
@@ -0,0 +1,13 @@
+$( document ).ready(function() {
+	$('#blog-link').click(function(){
+
+		$('#coming-soon-link').stop(true);
+
+		$('#coming-soon-link').fadeIn();
+
+		$('#coming-soon-link').fadeOut();
+
+	});
+
+	
+});

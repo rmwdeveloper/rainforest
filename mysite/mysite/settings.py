77d6bacf7fab2f179374ccf8a0eca17677eef6de
Robert Westenberger <rmwdeveloper@gmail.com>
2014-09-15 18:02:25 -0400

    Turned debug back on

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -55,9 +55,9 @@ try:
 except (NameError, AttributeError) as e:
 	SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 SITE_ID = 1
-TEMPLATE_DEBUG = False
+TEMPLATE_DEBUG = True
 
 # ALLOWED_HOSTS = ['127.0.0.1','127.0.0.1:8000', 'localhost']
 try:

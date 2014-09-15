0dcb68854f7be7737bf9816534ba80b6b4b744ce
Robert Westenberger <rmwdeveloper@gmail.com>
2014-09-15 18:53:07 -0400

    removed gzip middleware

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -85,7 +85,6 @@ INSTALLED_APPS = (
 )
 
 MIDDLEWARE_CLASSES = (
-	'django.middleware.gzip.GZipMiddleware',
 	'django.contrib.sessions.middleware.SessionMiddleware',
 	'django.middleware.common.CommonMiddleware',
 	'django.middleware.csrf.CsrfViewMiddleware',
